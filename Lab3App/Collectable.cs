5a6efb8721417259faae06d5f79cbd678895a9f5
Nicholas <nwoo5@uwo.ca>
2024-02-01 16:07:32 -0500

    Finished Lab 3

---
+++ Lab3App/Collectable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3App
+{
+    abstract class Collectable : Displayable
+    {
+        private protected string Description { get; set; }
+
+        public CollectionBoard Board;
+        public Collectable(string Description)
+        {
+            this.Description = Description;
+        }
+
+
+
+        public abstract void AddMe(List<Collectable> collectable);
+
+        public virtual void Display() { }
+
+    }
+}
---
+++ Lab3App/Tool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3App
+{
+    abstract class Tool : Collectable
+    {
+        public Tool(string Descripton) : base(Descripton) { }
+
+        public abstract void DoAction();
+
+        public override void AddMe(List<Collectable> collectable)
+        {
+            Console.WriteLine(this.Description + " Collected, Congrats!!!!");
+            DoAction();
+        }
+
+
+    }
+}
